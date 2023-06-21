1b52f8684ff1baa4a8888d5c50c62bb0aa8135b7
stefan-kanazir <kanazir.stefan@gmail.com>
2023-06-21 22:53:16 +0200

    Initial setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+};
+
+export default function About() {
+  return <h1>About Page</h1>;
+}
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+};
+
+export default function Portfolio() {
+  return <h1>Portfolio Page</h1>;
+}
